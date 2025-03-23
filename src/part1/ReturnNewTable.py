2bdd291fb7b9ffd9f57b5d371edcb67d8fc7fa98
harrisonoakess <oakeshjo00@gmail.com>
2025-03-23 01:36:33 -0600

    setup, and started transforming the file. I am pivoting the file so each population has its own row. This will help with calculations

---
+++ src/part1/ReturnNewTable.py
@@ -0,0 +1,20 @@
+# Percentage = (cell count for pupulation/total cell count) * 100
+
+import pandas as pd
+
+# Read the file in
+df = pd.read_csv("../../InstructionMaterial/cell-count.csv") 
+
+# Name the different population column headers
+populations = ['b_cell', 'cd8_t_cell', 'cd4_t_cell', 'nk_cell', 'monocyte']
+
+# Creates a new column that will sum up by row 
+df['total_count'] = df[populations].sum(axis=1)
+
+melt_df = df.melt(
+    id_vars=['sample', 'total_count'], # id_vars tells us which column to not change
+    value_vars=populations,           # value_vars tells us what column to pivot (make longer)
+    var_name='population',             # var_name gives a new name for population column
+    value_name='count'                 # value_name gives new name for cell counts
+)
+print(melt_df)
